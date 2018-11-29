4112c8561ec068b99f1f653fa6ecf837e100bac0
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2018-11-29 15:56:47 +0700

    new: add message and icon to empty view

--- featureweather/src/main/java/weather/feature/weather/empty/EmptyFragment.kt
+++ featureweather/src/main/java/weather/feature/weather/empty/EmptyFragment.kt
@@ -1,12 +1,16 @@
 package weather.feature.weather.empty
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.weather_icons_typeface_library.WeatherIcons
 import weather.feature.weather.R
 
 class EmptyFragment : Fragment() {
@@ -34,5 +38,12 @@ class EmptyFragment : Fragment() {
         view.findViewById<View>(R.id.searchCity).setOnClickListener {
             menuItem?.expandActionView()
         }
+        val value = TypedValue().apply {
+            view.context.theme.resolveAttribute(R.attr.colorAccent, this, true)
+        }
+        val icon = IconicsDrawable(view.context, WeatherIcons.Icon.wic_na)
+            .sizeDp(72)
+            .color(value.data)
+        view.findViewById<ImageView>(R.id.emptyIcon).setImageDrawable(icon)
     }
 }
