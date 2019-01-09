9b3346b1f39b3e9835a756a5422ec5523189b808
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2019-01-09 13:52:01 +0700

    prepare version 0.0.2-beta1

--- buildSrc/src/main/kotlin/WeatherProject.kt
+++ buildSrc/src/main/kotlin/WeatherProject.kt
@@ -6,8 +6,8 @@ object WeatherProject {
         const val compileSdkVersion = 28
         const val targetSdkVersion = 28
         const val applicationId = "cf.tukang.weather"
-        const val versionCode = 1
-        const val versionName = "0.0.1"
+        const val versionCode = 2
+        const val versionName = "0.0.2-beta1"
         const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
